83715e27560a209b18ee0060477fb1b5277ec276
raiankit1006 <115402024+raiankit1006@users.noreply.github.com>
2023-01-14 00:04:39 +0530

    1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args)
+    {
+        RWOnly a = new RWOnly();
+
+        a.setName("akki");
+        System.out.println(a.getName());
+
+        //name has private access in com.driver.RWOnly
+        //name has private access in com.driver.RWOnly
+    }
   
 }
